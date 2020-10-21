4a381a63358732ec7794043564c0b40ea13455ba
VolodymyrKoch <67268447+VolodymyrKoch@users.noreply.github.com>
2020-10-21 20:45:54 +0300

    виправив експорт і очистив попередню фото завантажену з модалки

--- js/task-1_moduly-8.js
+++ js/task-1_moduly-8.js
@@ -19,77 +19,14 @@
 // Перегортування зображень галереї у відкритому модальному вікні клавішами "вліво"
 // і "вправо".
 
-const array= [
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-];
+
+import gallery_items from './gallery_items.js';
 
 const ulListRef = document.querySelector(".js-gallery");
 const divWrapper = document.querySelector(".js-lightbox");
 const imgRef = document.querySelector('.lightbox__image');
 
-array.forEach((el) => {
+gallery_items.forEach((el) => {
   ulListRef.insertAdjacentHTML("afterbegin", `<li class="gallery__item">
   <a class="gallery__link" href="${el.original}"> 
    <img class="gallery__image" src="${el.preview}" 
@@ -105,7 +42,8 @@ ulListRef.addEventListener("click", (e) => {
     })
 
 const openModal = function (foto) {
-    divWrapper.classList.add("is-open");
+  divWrapper.classList.add("is-open");
+  imgRef.removeAttribute('src')
   imgRef.setAttribute("src", foto)
   }
 const btn = document.querySelector(".lightbox__button");
